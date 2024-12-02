cbb2cad00e346ec6820fd5f891685f9ac2b9a882
nicolewnchoi <wanpilc@umich.edu>
2024-12-01 22:39:37 -0500

    thumbnails horizontal

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-//product image slider
-var productThumb = new Swiper ('.small-image', {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 3,
-    freeMode: true,
-    watchSlidesProgress: true,
-    breakpoints: {
+document.addEventListener('DOMContentLoaded', function () {
+    var productThumb = new Swiper('.small-image', {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesProgress: true,
+      breakpoints: {
         481: {
-            spaceBetween: 32.
-        }
-    }
-});
-var productBig = new Swiper ('.big-image', {
-    loop: true,
-    autoHeight: true,
-    navigation: {
+          spaceBetween: 32,
+        },
+      },
+    });
+  
+    var productBig = new Swiper('.big-image', {
+      loop: true,
+      autoHeight: true,
+      navigation: {
         nextEl: '.swiper-button-next',
-        preEl: '.swiper-button-prev',
-    },
-    thumbs: {
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
         swiper: productThumb,
-        
-    }
-})
+      },
+    });
+  });
+  
+// //product image slider
+// var productThumb = new Swiper ('.small-image', {
+//     loop: true,
+//     spaceBetween: 10,
+//     slidesPerView: 3,
+//     freeMode: true,
+//     watchSlidesProgress: true,
+//     breakpoints: {
+//         481: {
+//             spaceBetween: 32.
+//         }
+//     }
+// });
+// var productBig = new Swiper ('.big-image', {
+//     loop: true,
+//     autoHeight: true,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         preEl: '.swiper-button-prev',
+//     },
+//     thumbs: {
+//         swiper: productThumb,
+
+//     }
+// })
