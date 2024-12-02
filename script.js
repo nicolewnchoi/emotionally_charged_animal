54afdbc64073932e5739c1246b7f91a4b02a3165
nicolewnchoi <wanpilc@umich.edu>
2024-12-02 14:02:44 -0500

    thumbnails vertical

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
       slidesPerView: 3,
       freeMode: true,
       watchSlidesProgress: true,
-      breakpoints: {
-        481: {
-          spaceBetween: 32,
-        },
-      },
     });
   
     var productBig = new Swiper('.big-image', {
@@ -24,6 +19,33 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     });
   });
+  
+// document.addEventListener('DOMContentLoaded', function () {
+//     var productThumb = new Swiper('.small-image', {
+//       loop: true,
+//       spaceBetween: 10,
+//       slidesPerView: 3,
+//       freeMode: true,
+//       watchSlidesProgress: true,
+//       breakpoints: {
+//         481: {
+//           spaceBetween: 32,
+//         },
+//       },
+//     });
+  
+//     var productBig = new Swiper('.big-image', {
+//       loop: true,
+//       autoHeight: true,
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       thumbs: {
+//         swiper: productThumb,
+//       },
+//     });
+//   });
   
 // //product image slider
 // var productThumb = new Swiper ('.small-image', {
